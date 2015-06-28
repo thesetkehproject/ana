c9c08c7738ac9960ad0a4277c7ed915f41837bdc
Brian Tomlinson <darthlukan@gmail.com>
2015-06-28 13:47:08 +0200

    strip it down to nothing, we'll expand as needed

--- irc/irc.go
+++ irc/irc.go
@@ -1,16 +1,17 @@
-// Ana
 package irc
 
 import (
 	"encoding/json"
 	"fmt"
 	"github.com/thesetkehproject/ana/logger"
-	"github.com/thoj/go-ircevent"
+	irclib "github.com/thoj/go-ircevent"
 	"os"
-	"strings"
 )
 
-const delay = 40
+var (
+	config     *Config
+	connection *irclib.Connection
+)
 
 type Config struct {
 	Admins     []string
@@ -26,47 +27,16 @@ type Config struct {
 	Forums     string
 }
 
-// ParseCmds takes PRIVMSG strings containing a preceding bang "!"
-// and attempts to turn them into an ACTION that makes sense.
-// Returns a msg string.
-func ParseCmds(cmdMsg string, config *Config) string {
-	var (
-		msg      string
-		msgArray []string
-		cmdArray []string
-	)
-
-	cmdArray = strings.SplitAfterN(cmdMsg, config.Trigger, 2)
-
-	if len(cmdArray) > 0 {
-		msgArray = strings.SplitN(cmdArray[1], " ", 2)
-	}
-
-	switch {
-	case strings.Contains(msgArray[0], "help"):
-		msg = HelpCmd(config.Trigger)
-	case strings.Contains(msgArray[0], "wiki"):
-		msg = WikiCmd(config)
-	case strings.Contains(msgArray[0], "homepage"):
-		msg = HomePageCmd(config)
-	case strings.Contains(msgArray[0], "forums"):
-		msg = ForumCmd(config)
-	default:
-		msg = ""
-	}
-	return msg
-}
-
 // AddCallbacks is a single function that does what it says.
 // It's merely a way of decluttering the main function.
-func AddCallbacks(conn *irc.Connection, config *Config) {
+func AddCallbacks(conn *irclib.Connection, config *Config) {
 	log := fmt.Sprintf("%s%s", config.LogDir, config.Channel)
 
-	conn.AddCallback("001", func(e *irc.Event) {
+	conn.AddCallback("001", func(e *irclib.Event) {
 		conn.Join(config.Channel)
 	})
 
-	conn.AddCallback("JOIN", func(e *irc.Event) {
+	conn.AddCallback("JOIN", func(e *irclib.Event) {
 		if e.Nick == config.BotNick {
 			conn.Privmsg(config.Channel, "Hello everybody, I'm a bot")
 			logger.LogDirCheck(config.LogDir)
@@ -75,42 +45,9 @@ func AddCallbacks(conn *irc.Connection, config *Config) {
 		message := fmt.Sprintf("%s has joined", e.Nick)
 		go logger.IRCChannelLogger(log, e.Nick, message)
 	})
-	conn.AddCallback("PART", func(e *irc.Event) {
-		message := fmt.Sprintf("has parted (%s)", e.Message())
-		nick := fmt.Sprintf("%s@%s", e.Nick, e.Host)
-		go logger.IRCChannelLogger(log, nick, message)
-	})
-	conn.AddCallback("QUIT", func(e *irc.Event) {
-		message := fmt.Sprintf("has quit (%v)", e.Message)
-		nick := fmt.Sprintf("%s@%s", e.Nick, e.Host)
-		go logger.IRCChannelLogger(log, nick, message)
-	})
-
-	conn.AddCallback("PRIVMSG", func(e *irc.Event) {
-		var response string
-		message := e.Message()
-		if strings.Contains(message, config.Trigger) && strings.Index(message, config.Trigger) == 0 {
-			response = ParseCmds(message, config)
-		}
-
-		if strings.Contains(message, fmt.Sprintf("%squit", config.Trigger)) {
-			QuitCmd(config.Admins, e.Nick)
-		}
-
-		if len(response) > 0 {
-			conn.Privmsg(config.Channel, response)
-		}
-
-		if len(message) > 0 {
-			if e.Arguments[0] != config.BotNick {
-				go logger.IRCChannelLogger(log, e.Nick+": ", message)
-			}
-		}
-	})
 }
 
-func main() {
-
+func init() {
 	file, err := os.Open("config.json")
 
 	if err != nil {
@@ -120,17 +57,19 @@ func main() {
 	defer file.Close()
 
 	decoder := json.NewDecoder(file)
-	config := &Config{}
+	config = &Config{}
 	decoder.Decode(&config)
 
-	conn := irc.IRC(config.BotNick, config.BotUser)
-	err = conn.Connect(config.Server)
+	connection := irclib.IRC(config.BotNick, config.BotUser)
+	err = connection.Connect(config.Server)
 
 	if err != nil {
 		fmt.Println("Failed to connect.")
 		panic(err)
 	}
+}
 
-	AddCallbacks(conn, config)
-	conn.Loop()
+func Client() {
+	AddCallbacks(connection, config)
+	connection.Loop()
 }
