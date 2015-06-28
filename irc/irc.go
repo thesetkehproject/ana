2c065c652bf40d01005cd7237b580dda05a03e5e
Brian Tomlinson <darthlukan@gmail.com>
2015-06-28 13:58:44 +0200

    further strip-down, TODO

--- irc/irc.go
+++ irc/irc.go
@@ -3,7 +3,6 @@ package irc
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/thesetkehproject/ana/logger"
 	irclib "github.com/thoj/go-ircevent"
 	"os"
 )
@@ -27,24 +26,10 @@ type Config struct {
 	Forums     string
 }
 
-// AddCallbacks is a single function that does what it says.
-// It's merely a way of decluttering the main function.
-func AddCallbacks(conn *irclib.Connection, config *Config) {
-	log := fmt.Sprintf("%s%s", config.LogDir, config.Channel)
-
-	conn.AddCallback("001", func(e *irclib.Event) {
-		conn.Join(config.Channel)
-	})
-
-	conn.AddCallback("JOIN", func(e *irclib.Event) {
-		if e.Nick == config.BotNick {
-			conn.Privmsg(config.Channel, "Hello everybody, I'm a bot")
-			logger.LogDirCheck(config.LogDir)
-			logger.LogFile(config.LogDir + e.Arguments[0])
-		}
-		message := fmt.Sprintf("%s has joined", e.Nick)
-		go logger.IRCChannelLogger(log, e.Nick, message)
-	})
+func SendNotification(target, message string) {
+	connection.Notice(target, message)
+	// TODO: Logger needs to be able to log more than just a channel, but notices as well
+	fmt.Printf("Wrote message: '%s' to target '%s'", message, target)
 }
 
 func init() {
@@ -70,6 +55,5 @@ func init() {
 }
 
 func Client() {
-	AddCallbacks(connection, config)
 	connection.Loop()
 }
