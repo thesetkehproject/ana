a815bf83592df37a6f06b1b5f28736ea7b75cb83
Brian Tomlinson <darthlukan@gmail.com>
2015-06-28 15:06:45 +0200

    new generic logging function

--- logger/logging.go
+++ logger/logging.go
@@ -8,6 +8,28 @@ import (
 	"time"
 )
 
+func LogDirCheck(logdir string) {
+	if _, err := os.Stat(logdir); os.IsNotExist(err) {
+		fmt.Printf("No such file or directory: %s, creating.\n", logdir)
+		os.Mkdir(logdir, 0777)
+	} else {
+		fmt.Printf("%s exists.\n", logdir)
+	}
+}
+
+func LogFileCheck(logfile string) {
+	prelog := strings.Replace(logfile, "#", "", 1)
+	file := fmt.Sprintf("%s.log", prelog)
+
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		fmt.Printf("Log file '%s' does not Exist. Creating.\n", file)
+		os.Create(file)
+		fmt.Printf("Log file '%s' created.\n", file)
+	} else {
+		fmt.Printf("Log file '%s' exists, reusing.\n", file)
+	}
+}
+
 func IRCChannelLogger(logfile string, nick string, message string) {
 	STime := time.Now().UTC().Format(time.ANSIC)
 	prefile := strings.Replace(logfile, "#", "", 1)
@@ -25,24 +47,17 @@ func IRCChannelLogger(logfile string, nick string, message string) {
 	}
 }
 
-func LogDirCheck(logdir string) {
-	if _, err := os.Stat(logdir); os.IsNotExist(err) {
-		fmt.Printf("No such file or directory: %s, creating.\n", logdir)
-		os.Mkdir(logdir, 0777)
-	} else {
-		fmt.Printf("%s exists.\n", logdir)
+func GenericLogger(destination, message string) error {
+	t := time.Now().UTC().Format(time.ANSIC)
+	dest, err := os.OpenFile(destination, os.O_RDWR|os.O_APPEND|os.O_SYNC, 0666)
+	if err != nil {
+		return err
 	}
-}
+	defer dest.Close()
 
-func LogFile(logfile string) {
-	prelog := strings.Replace(logfile, "#", "", 1)
-	file := fmt.Sprintf("%s.log", prelog)
-
-	if _, err := os.Stat(file); os.IsNotExist(err) {
-		fmt.Printf("Log file '%s' does not Exist. Creating.\n", file)
-		os.Create(file)
-		fmt.Printf("Log file '%s' created.\n", file)
-	} else {
-		fmt.Printf("Log file '%s' exists, reusing.\n", file)
+	_, err = io.WriteString(dest, fmt.Sprintf("%v: %s\n", t, message))
+	if err != nil {
+		return err
 	}
+	return nil
 }
